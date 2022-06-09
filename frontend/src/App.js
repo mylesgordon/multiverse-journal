26352373b678ca382ee39809c28b0b4250d24180
Myles Gordon <myles.gordon@sky.uk>
2022-06-09 13:40:43 +0100

    Moved Home to pages directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import Header from "./components/Header";
-import Home from "./components/Home";
+import Header from "./pages/Header";
+import Home from "./pages/Home";
 
 function App() {
     return (
